61c79ee4ab282707709e14e3cec0a1529768a7c8
adriplaalonso-lgtm <adriplaalonso@gmail.com>
2026-09-23 14:45:39 +0200

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,118 @@
+const CURRUSCOS_DATA = {
+
+    members: [
+        {
+            id: "espinete",
+            name: "Espinete",
+            username: "espinete",
+            role: "Miembro",
+            image: "assets/img/espinete.jpg"
+        },
+        {
+            id: "srcarlos",
+            name: "srcarlos",
+            username: "srcarlos",
+            role: "Miembro",
+            image: "assets/img/srcarlos.jpg"
+        },
+        {
+            id: "alvox",
+            name: "Alvox",
+            username: "alvox",
+            role: "Miembro",
+            image: "assets/img/alvox.jpg"
+        },
+        {
+            id: "adyleko",
+            name: "Adyleko",
+            username: "adyleko",
+            role: "Miembro",
+            image: "assets/img/adyleko.jpg"
+        },
+        {
+            id: "anra",
+            name: "Anra",
+            username: "anra",
+            role: "Miembro",
+            image: "assets/img/anra.jpg"
+        },
+        {
+            id: "crisgg",
+            name: "Crisgg",
+            username: "crisgg",
+            role: "Miembro",
+            image: "assets/img/crisgg.jpg"
+        },
+        {
+            id: "barlac",
+            name: "Barlac",
+            username: "barlac",
+            role: "Miembro",
+            image: "assets/img/barlac.jpg"
+        },
+        {
+            id: "guillito",
+            name: "Guillito",
+            username: "mrchapo",
+            role: "Mr. Chapó",
+            image: "assets/img/guillito.jpg"
+        }
+    ],
+
+    quotes: [
+        {
+            text: "Voy a entrar.",
+            author: "Mr. Chapó"
+        },
+        {
+            text: "Antojito, hijos de puta.",
+            author: "Curruscos Clan"
+        },
+        {
+            text: "Frase histórica del clan.",
+            author: "Curruscos Clan"
+        }
+    ],
+
+    memories: [
+        {
+            id: 1,
+            title: "El comienzo",
+            date: "2024-09-01",
+            description: "Todo empezó en algún momento.",
+            image: "assets/img/memoria1.jpg"
+        },
+        {
+            id: 2,
+            title: "Una noche más",
+            date: "2025-06-14",
+            description: "Otra historia que probablemente no debería estar aquí.",
+            image: "assets/img/memoria2.jpg"
+        },
+        {
+            id: 3,
+            title: "Santander",
+            date: "2026-07-20",
+            description: "Dos días que merecen ser recordados.",
+            image: "assets/img/memoria3.jpg"
+        }
+    ],
+
+    events: [
+        {
+            id: 1,
+            title: "Próxima quedada",
+            date: "2026-09-28",
+            location: "Palencia",
+            description: "Otra quedada del clan."
+        },
+        {
+            id: 2,
+            title: "Cumpleaños",
+            date: "2026-10-12",
+            location: "Palencia",
+            description: "Celebración del clan."
+        }
+    ]
+
+};
